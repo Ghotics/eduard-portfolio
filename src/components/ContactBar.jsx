1ecd9542788ad83320f01b51198c4ae72c29e535
Eduard Tudorache <edy_edytza2003@yahoo.com>
2023-02-17 21:28:24 +0100

    started work on contact bar

---
+++ src/components/ContactBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ContactBar.scss'
+
+const ContactBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Github</li>
+        <li>LinkedIn</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactBar;
