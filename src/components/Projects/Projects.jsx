d2aaf83f8c90250ed35cf560a02aa7923d877eab
Ghotics <edy_edytza2003@yahoo.com>
2023-03-13 18:20:56 +0100

    project list

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,11 @@ const Projects = () => {
     return (
         <div>
             <h1 className='title-projects'> Projects </h1>
+            <ol>
+                <li></li>\
+                <li></li>
+                <li></li>
+            </ol>
         </div>
     );
 };
